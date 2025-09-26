5ba154a110188393cfcfae0f56d577324193bdbb
ClaireParker907 <claire.parker024@gmail.com>
2025-09-25 19:46:14 -0600

    mission 2

---
+++ mission/mission.js
@@ -0,0 +1,42 @@
+// With JavaScript select the select element out of the DOM.
+const themeSelector = document.querySelector('#theme-selector');
+const bodyElement = document.querySelector('body');
+const logoElement = document.querySelector('#logo');
+
+// Define the file names for the logos
+const BLUE_LOGO_SRC = "byui-logo_blue.webp";
+const WHITE_LOGO_SRC = "byui-logo_white.png";
+
+/**
+ * Handles the theme change event.
+ * Toggles the 'dark' class on the body and swaps the logo image.
+ */
+function changeTheme() {
+    // Check to see what the current value of our select is.
+    const selectedTheme = themeSelector.value;
+
+    if (selectedTheme === "dark") {
+        // If the value is dark then:
+        // 1. Add the dark class to the body
+        bodyElement.classList.add('dark');
+        
+        // 2. Change the source of the logo img to point to the white logo.
+        logoElement.src = WHITE_LOGO_SRC;
+    } else {
+        // Otherwise:
+        // 1. Remove the dark class
+        bodyElement.classList.remove('dark');
+        
+        // 2. Make sure the logo src is the blue logo.
+        logoElement.src = BLUE_LOGO_SRC;
+    }
+}
+// Add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
+
+
+
+
+
+
