e1b2fbea5bca0f2b16db77266b3beda93958f7c4
ClaireParker907 <claire.parker024@gmail.com>
2025-10-08 08:29:06 -0600

    ponder

---
+++ Ponder/W04/debugging.js
@@ -0,0 +1,20 @@
+// - ACtiViTY 1
+// const PI = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * PI;
+// console.log("Area1:",area);
+// radius = 4;
+// area = radius * radius * PI;
+// counsole.log("Area2:", area)
+
+// - ACTIVITY 2
+const PI = 3.14;
+let area = 0;
+
+function circleArea(radius) {
+  const area = radius * radius * PI;
+  return area
+}
+area = circleArea(3);
+console.log(area);
